5d04f39fa74bdb7a85e48d49c0c762fc05dbd1c2
Vibhas <vkvats@iu.edu>
2020-05-20 12:55:06 -0400

    week 2 recode completed

--- Specialization-2/Week2-assignment/dataloader.py
+++ Specialization-2/Week2-assignment/dataloader.py
@@ -7,6 +7,16 @@ import sklearn.datasets
 import sklearn.linear_model
 import scipy.io
 
+# def load_dataset():
+#     np.random.seed(3)
+#     train_X, train_Y = sklearn.datasets.make_moons(n_samples=300, noise=.2) #300 #0.2 
+#     # Visualize the data
+#     plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
+#     train_X = train_X.T
+#     train_Y = train_Y.reshape((1, train_Y.shape[0]))
+    
+    # return train_X, train_Y
+
 def load_2D_dataset():
     data = scipy.io.loadmat('datasets/data.mat')
     train_X = data['X'].T
